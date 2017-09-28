12721f6cf319f854c518aeb45df309f797356ccc
hordan <daniilhorbach@gmail.com>
2017-09-28 22:51:37 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -19,11 +19,11 @@ class TicTacToe {
             this.pole[rowIndex][columnIndex] = this.symbol;
             if (this.symbol === 'o') {
                 this.symbol = 'x';
-                return;
+            
             }
             if (this.symbol === 'x') {
                 this.symbol = 'o';
-                return;
+    
             }
         }
     }
@@ -37,7 +37,7 @@ class TicTacToe {
 
     getWinner() {
         for (var i = 0; i < 3; i++) {
-            if ((this.pole[i][0] === this.pole[i][1]) &&( this.pole[i][1] === this.pole[i][2]) && (this.pole[i][2] !== null)) {
+            if ((this.pole[i][0] === this.pole[i][1]) &&( this.pole[i][1] === this.pole[i][2]) && (this.pole[i][0] !== null)) {
                 return this.pole[i][0];
             }
         }

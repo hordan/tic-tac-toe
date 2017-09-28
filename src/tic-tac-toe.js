3b992b031d214db2f468ea5840b9697e8cc42764
hordan <daniilhorbach@gmail.com>
2017-09-29 00:24:58 +0300

    work

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,13 +1,13 @@
 class TicTacToe {
     constructor() {
+        this.symbol= 'x';
         this.pole = [];
-        for (var i = 0; i < 3; i++) {
-            this.pole[i] = [];
-            for (var j = 0; j < 3; j++) {
-                this.pole[i][j] = null;
-            }
+        for(var i = 0; i < 3; i++){
+          this.pole[i] = [];
+          for(var j = 0; j < 3; j++){
+            this.pole[i][j] = null;
+          }
         }
-        this.symbol = 'x';
     }
 
     getCurrentPlayerSymbol() {
@@ -15,64 +15,67 @@ class TicTacToe {
     }
 
     nextTurn(rowIndex, columnIndex) {
-        if (this.pole[rowIndex][columnIndex] === null) {
-            this.pole[rowIndex][columnIndex] = this.symbol;
-            if (this.symbol === 'o') {
-                this.symbol = 'x';
-
-            }
-            if (this.symbol === 'x') {
-                this.symbol = 'o';
-
-            }
+        if(this.pole[rowIndex][columnIndex] === null){
+          this.pole[rowIndex][columnIndex] = this.symbol;
+          if(this.symbol === 'x') {
+            this.symbol = 'o';
+          } else {
+            this.symbol= 'x';
+          }
         }
     }
 
     isFinished() {
-        if ((this.getWinner() !== null) || (this.isDraw() === true)) {
-            return true;
+        if((this.getWinner() !== null) || (this.isDraw() === true)){
+          return true;
         }
+
         return false;
     }
 
     getWinner() {
         for (let i = 0; i < 3; i++) {
-            if ((this.pole[i][0] === this.pole[i][1]) &&( this.pole[i][1] === this.pole[i][2]) && (this.pole[i][0] !== null)) {
-                return this.pole[i][0];
-            }
+          if((this.pole[i][0] === this.pole[i][1]) && (this.pole[i][1] === this.pole[i][2]) && (this.pole[i][0] !== null))
+          {
+            return this.pole[i][0];
+          }
         }
 
         for (let i = 0; i < 3; i++) {
-            if ((this.pole[0][i] === this.pole[1][i] )&&(this.pole[1][i] === this.pole[2][i] )&&( this.pole[0][i] !== null )) {
-                return this.pole[0][i];
-            }
+          if((this.pole[0][i] === this.pole[1][i]) && (this.pole[1][i] === this.pole[2][i]) && (this.pole[0][i] !== null))
+          {
+            return this.pole[0][i];
+          }
         }
 
-        if ((this.pole[0][0] === this.pole[1][1]) &&( this.pole[1][1] === this.pole[2][2]) &&( this.pole[0][0] !== null )) {
-            return this.pole[0][0];
+        if((this.pole[0][0] === this.pole[1][1]) && (this.pole[1][1] === this.pole[2][2]) && (this.pole[0][0] !== null))
+        {
+          return this.pole[0][0];
         }
 
-        if ((this.pole[0][2] === this.pole[1][1] )&& (this.pole[1][1] === this.pole[2][0] )&&( this.pole[0][2] !== null ) ) {
-            return pole[0][2];
+        if((this.pole[0][2] === this.pole[1][1]) && (this.pole[1][1] === this.pole[2][0]) && (this.pole[0][2] !== null))
+        {
+          return this.pole[0][2];
         }
+
         return null;
-
     }
 
     noMoreTurns() {
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                if (this.pole[i][j] === null) {
-                    return false;
-                }
+      for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) {
+            if (this.pole[i][j] === null ) {
+              return false;
             }
+          }
         }
+
         return true;
     }
 
     isDraw() {
-        if ((this.noMoreTurns()) && (this.getWinner() === null)) {
-            return true;
+        if((this.noMoreTurns()) && (this.getWinner() === null)){
+          return true;
         }
 
         return false;
